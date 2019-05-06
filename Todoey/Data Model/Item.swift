772f4f31e8df5f85b92136e1f77ed3e379deddcd
Pekshn <petar034@hotmail.com>
2019-05-07 00:25:48 +0200

    convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by user on 5/6/19.
+//  Copyright © 2019 Pekshn. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
